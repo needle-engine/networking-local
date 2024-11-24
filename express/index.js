5b95dcfce8564bd4893c405d91f14eaa61da5a58
hybridherbst <felix.herbst@gmail.com>
2024-11-24 21:50:47 +0100

    allow connecting to a different endpoint, allow querying all rooms in development mode

--- express/index.js
+++ express/index.js
@@ -27,11 +27,22 @@ const websocketEndpoint = "/socket";
 const networking = require("@needle-tools/networking");
 networking.startServerExpress(app, { server: server, endpoint: websocketEndpoint });
 
-/**
- * @type {string|undefined|number}
- */
+// For local development, we can expose the room list
+// (if stored locally as JSON files) for easier inspection.
+if (process.env.NEEDLE_NETWORKING_EXPOSE_ROOM_ROUTE && !process.env.NEEDLE_NETWORKING_S3_ENDPOINT) {
+  const { readdir } = require("fs/promises");
+  const Path = require("path");
+  app.get("/rooms", async (_req, res) => {
+    const files = await readdir(".data/rooms");
+    const sorted = files.map(f => Path.parse(f).name).sort();
+    res.json(sorted);
+  });
+}
+
+/** @type {string|undefined|number} */
 let port = process.env.PORT;
 if (!port?.length) port = 9001;
+
 const listener = server.listen(port, function () {
   const address = listener.address();
   if (!address) {
